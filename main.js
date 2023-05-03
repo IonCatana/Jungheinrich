2feb102c1fff554d79536e37900364059429942f
Ion Catana <90708252+IonCatana@users.noreply.github.com>
2023-05-03 11:29:38 +0200

    add footer

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
 // Changing color of the element button when i write 1 character or more
-$(function () {
-  const inputElem = $('section#input-code .container .contain .column-right .input input');
-  const buttonElem = $('section#input-code .container .contain .column-right .input button');
 
-  inputElem.on('input', function () {
-    const firstChar = $(this).val().charAt(0);
+const inputElem = $('section#input-code .container .contain .column-right .input input');
+const buttonElem = $('section#input-code .container .contain .column-right .input button');
 
-    // Sets the background-color of the button element to white if the first character of the input value is truthy
-    buttonElem.css('background-color', firstChar ? 'white' : '');
-  });
-  
+inputElem.on('input', function () {
+  const firstChar = $(this).val().charAt(0);
+
+  // Sets the background-color of the button element to white if the first character of the input value is truthy
+  buttonElem.css('background-color', firstChar ? 'white' : '');
 });
 
 
 
-// $(function () {
-//   let $buttons = $('section#evolution .container .contain .buttons .button');
-//   $buttons.on('click', function () {
-//     // Remove .active class from all buttons
-//     $buttons.removeClass('active');
 
-//     // Add .active to the clicked button
-//     $(this).addClass('active');
-//   });
-// });
-
-const buttons =document.querySelectorAll('.button');
+// SECTION: EVOLUTION
+const buttons = document.querySelectorAll('section#evolution .container .contain .buttons .button');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     buttons.forEach(btn => btn.classList.remove('active'));
     button.classList.add('active');
   })
+});
+
+
+//Footer Anchor Link scolling to the header section
+const anchorLink = document.querySelector('footer.egloo .container .avon-logo .anchor-link');
+
+anchorLink.addEventListener('click', () => {
+  const header = document.querySelector('header');
+  header.scrollIntoView({
+    behavior: 'smooth'
+  });
 });
 
 
